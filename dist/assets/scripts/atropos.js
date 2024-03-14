cf9653928b04d6e442f71ae2260a03970d8a9709
TruongDuyKhai <truongduykhai1012@gmail.com>
2024-03-14 15:29:22 +0700

    .

---
+++ dist/assets/scripts/atropos.js
@@ -0,0 +1,19 @@
+function loadAtropos() {
+  Atropos({
+    el: '.atropos-card',
+    duration: 400,
+    activeOffset: 70,
+    shadow: true,
+    shadowScale: 1,
+    highlight: true,
+    // onEnter() {
+    //   console.log('Enter');
+    // },
+    // onLeave() {
+    //   console.log('Leave');
+    // },
+    // onRotate(x, y) {
+    //   // console.log('Rotate', x, y);
+    // }
+  });
+}
